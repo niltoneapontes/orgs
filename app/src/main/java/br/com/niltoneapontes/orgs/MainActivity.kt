35c2c1976fc7bb4c372806522f59bfbcc0bc05a3
Nilton <niltoneapontes@gmail.com>
2023-03-30 17:11:28 -0300

    feat: create recycleview and product layout

--- app/src/main/java/br/com/niltoneapontes/orgs/MainActivity.kt
+++ app/src/main/java/br/com/niltoneapontes/orgs/MainActivity.kt
@@ -2,11 +2,20 @@ package br.com.niltoneapontes.orgs
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity: Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Toast.makeText(this, "Bem vinde ao Orgs", Toast.LENGTH_LONG).show()
+        setContentView(R.layout.activity_main)
+        val name: TextView = findViewById<TextView>(R.id.title)
+        val description: TextView = findViewById<TextView>(R.id.description)
+        val value: TextView = findViewById<TextView>(R.id.value)
+
+        name.text = "Cesta de frutas"
+        description.text = "Laranja, manga e pera"
+        value.text = "R$ 30,00"
     }
 }
