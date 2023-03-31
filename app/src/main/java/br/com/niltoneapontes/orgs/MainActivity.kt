a52907be3972322c3461aa2f89e569ce4e90b45a
Nilton <niltoneapontes@gmail.com>
2023-03-31 11:31:01 -0300

    feat: create recycleview adapter

--- app/src/main/java/br/com/niltoneapontes/orgs/MainActivity.kt
+++ app/src/main/java/br/com/niltoneapontes/orgs/MainActivity.kt
@@ -5,17 +5,23 @@ import android.os.Bundle
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import br.com.niltoneapontes.orgs.ui.recyclerview.adapter.ListProductsAdapter
 
 class MainActivity: Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val name: TextView = findViewById<TextView>(R.id.title)
-        val description: TextView = findViewById<TextView>(R.id.description)
-        val value: TextView = findViewById<TextView>(R.id.value)
+//        val name: TextView = findViewById<TextView>(R.id.title)
+//        val description: TextView = findViewById<TextView>(R.id.description)
+//        val value: TextView = findViewById<TextView>(R.id.value)
+//
+//        name.text = "Cesta de frutas"
+//        description.text = "Laranja, manga e pera"
+//        value.text = "R$ 30,00"
 
-        name.text = "Cesta de frutas"
-        description.text = "Laranja, manga e pera"
-        value.text = "R$ 30,00"
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+
+        recyclerView.adapter = ListProductsAdapter()
     }
 }
---
+++ app/src/main/java/br/com/niltoneapontes/orgs/ui/recyclerview/adapter/Product.kt
@@ -0,0 +1,9 @@
+package br.com.niltoneapontes.orgs.ui.recyclerview.adapter
+
+import java.math.BigDecimal
+
+class Product(
+    val name: String,
+    val description: String,
+    val value: BigDecimal
+)
