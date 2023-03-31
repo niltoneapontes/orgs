dd4a57bdcb0d3055d34ac55346ed798958758d3a
Nilton <niltoneapontes@gmail.com>
2023-03-31 14:13:33 -0300

    feat: implemented adapter methods

--- app/src/main/java/br/com/niltoneapontes/orgs/MainActivity.kt
+++ app/src/main/java/br/com/niltoneapontes/orgs/MainActivity.kt
@@ -5,10 +5,13 @@ import android.os.Bundle
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import br.com.niltoneapontes.orgs.ui.recyclerview.adapter.ListProductsAdapter
+import br.com.niltoneapontes.orgs.ui.recyclerview.adapter.Product
+import java.math.BigDecimal
 
-class MainActivity: Activity() {
+class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,6 +25,25 @@ class MainActivity: Activity() {
 
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
 
-        recyclerView.adapter = ListProductsAdapter()
+        recyclerView.adapter = ListProductsAdapter(
+            context = this, products = listOf(
+                Product(
+                    name = "Cesta de frutas",
+                    description = "Laranja, manga e uva",
+                    value = BigDecimal("19.90")
+                ),
+                Product(
+                    name = "Cesta de frutas 2",
+                    description = "Laranja, maçã e uva",
+                    value = BigDecimal("19.90")
+                ),
+                Product(
+                    name = "Cesta de frutas 3",
+                    description = "Laranja, pera e uva",
+                    value = BigDecimal("19.90")
+                )
+            )
+        )
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
